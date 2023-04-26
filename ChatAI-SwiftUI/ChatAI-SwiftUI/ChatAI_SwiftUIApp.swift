2301441fff113a17616650e9d7948f2418abadbe
Barış Özgen <barisozgen@hotmail.com>
2023-04-26 16:09:36 +0400

    init project

---
+++ ChatAI-SwiftUI/ChatAI-SwiftUI/ChatAI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAI_SwiftUIApp.swift
+//  ChatAI-SwiftUI
+//
+//  Created by Baris OZGEN on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
