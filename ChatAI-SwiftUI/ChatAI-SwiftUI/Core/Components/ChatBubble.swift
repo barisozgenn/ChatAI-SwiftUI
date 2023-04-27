63dc3e9e24ea3e3e06a487ee3527bd76f93902ad
Barış Özgen <barisozgen@hotmail.com>
2023-04-27 12:39:14 +0400

    chat bubble view ui done

--- ChatAI-SwiftUI/ChatAI-SwiftUI/Core/Components/ChatBubble.swift
+++ ChatAI-SwiftUI/ChatAI-SwiftUI/Core/Components/ChatBubble.swift
@@ -8,30 +8,52 @@
 import SwiftUI
 
 struct ChatBubble: View {
-    @State var title : String = "BarisOzgen"
-    @State var message: String = "Message will be shown here"
+    
+    @State var isAI : Bool = false
+    @State var message: String = "Baris Message will be shown here"
+    @State var imageUrl: String = ""
     var body: some View {
-        VStack{
-            MessageBubble(direction: .right) {
+        HStack(spacing: 0){
+            if(!isAI){
+                paddingBalloon
+            }
+            
+            MessageBubble(direction: isAI ? .left : .right) {
                 VStack(alignment: .leading){
-                    Text(title)
+                    Text(isAI ? "AI" : "You")
                         .font(.subheadline)
                         .fontWeight(.bold)
                     Text(message)
                         .font(.headline)
                         .fontWeight(.regular)
+                    if !imageUrl.isEmpty{
+                        
+                        Image("aiAssistant")
+                            .resizable()
+                            .scaledToFit()
+                    }
                 }
-                              
-                                    .padding(.all, 20)
-                                    .padding(.horizontal)
-                                    .foregroundColor(Color.black)
-                                    .background(Color(.systemGray5))
-                                    
-                            }
+                
+                .padding(.all, 20)
+                .padding(.horizontal)
+                .foregroundColor(isAI ? .black : .white)
+                .background(isAI ? Color(.systemGray5) : .blue)
+                
+            }
+            
+            if(isAI){
+                paddingBalloon
+            }
         }
     }
 }
-
+extension ChatBubble {
+    var paddingBalloon: some View {
+        HStack{}
+            .frame(width: 29, height: 14)
+            .padding(0)
+    }
+}
 struct ChatBubble_Previews: PreviewProvider {
     static var previews: some View {
         ChatBubble()
